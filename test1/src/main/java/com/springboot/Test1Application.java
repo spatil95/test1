c28acea09e4c890c60b360ec1eead31967c57696
sagar <sagar@idea-PC>
2020-08-03 01:43:31 +0530

    ss

--- test1/src/main/java/com/springboot/Test1Application.java
+++ test1/src/main/java/com/springboot/Test1Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
-		//ss
+		//sagar
 		SpringApplication.run(Test1Application.class, args);
 	}
 
