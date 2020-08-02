bdc1274b0dc3ef6d98c29880d4dcc416354e7a06
sagar <sagar@idea-PC>
2020-08-02 21:35:16 +0530

    ss

--- test1/src/main/java/com/springboot/Test1Application.java
+++ test1/src/main/java/com/springboot/Test1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test1Application {
 
 	public static void main(String[] args) {
+		//ss
 		SpringApplication.run(Test1Application.class, args);
 	}
 
